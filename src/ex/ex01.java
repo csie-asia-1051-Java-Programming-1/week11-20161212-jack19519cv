40e1cf3f0c38f93ddd775eb56f50e13d3a74207e
jack19519cv <105021059@live.asia.edu.tw>
2016-12-18 21:49:37 +0800

    save

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,10 +1,58 @@
 package ex;
+
+import java.util.Scanner;
+
 /*
  * Date: 2016/12/12
- * Author: 105021***  ???
+ * Author: 105021059  ¤ý«T÷g
  */
 public class ex01 {
 	public static void main(String[] args) {
-
+		Scanner scn = new Scanner(System.in);
+		int n =scn.nextInt();
+		
+		int s=0;
+		for(int i =0;i<n;i++){
+			
+			
+			int t=scn.nextInt();
+		int m=scn.nextInt();
+		int k=scn.nextInt();
+		
+		for(int j=0;j<k;j++){
+			int l=scn.nextInt();
+			s=x(l,t);
+		}
+		
+			
+			
+			
+		if(m>k&&k>100){
+			
+		}
+		System.out.print(s);
+			
+		}
+		
 	}
+	
+	public static int x(int a,int b ){
+
+		int s=0;
+		if(a<b){
+			s+=a;
+		}
+		if(s>b){
+			s=0;}
+		
+		return s;
+	}
+
+public static void v(int m,int k ){
+	if(m>k&&k>100){
+		System.out.print("Impossible");
+	}
+	
+	
+}
 }
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,10 +1,25 @@
 package ex;
+
+import java.util.Scanner;
+
 /*
  * Date: 2016/12/12
  * Author: 105021***  ???
  */
 public class ex02 {
 	public static void main(String[] args) {
-
+		Scanner scn = new Scanner(System.in);
+		int a=0;
+		boolean x=true;
+		while(x){
+        int n=scn.nextInt();
+      a++;
+      if(n>0&&n==0&&n<0){
+    	  continue;
+      }else{
+      x=false;
+      }	}
+		System.out.print(a);
+        
 	}
 }
